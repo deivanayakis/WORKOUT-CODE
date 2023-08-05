28808e2191ab3645aa80033a2741e80e0e796fd8
Code Wizard <132194411+deivanayakis@users.noreply.github.com>
2023-08-05 22:49:22 +0530

    Add files via upload

---
+++ HourGlassPattern.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+      int i,j,n;
+      printf("Enter number of rows :");
+      scanf("%d",&n);
+      for(i=n-1;i>=0;i--)
+      {
+         for(j=0;j<n-i;j++)
+         {
+            printf(" ");
+         }
+         for(j=0;j<=i;j++)
+         {
+            printf("* ");
+         }
+         printf("\n");
+      }
+      for(i=1;i<n;i++)
+      {
+         for(j=0;j<n-i;j++)
+         {
+            printf(" ");
+         }
+         for(j=0;j<=i;j++)
+         {
+            printf("* ");
+         }
+         printf("\n");
+      }
+      return 0;
+}
